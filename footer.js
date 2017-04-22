22efd85a5c5c667abc1163417a52622d21da4d74
bitzlf <zlf741@gmail.com>
2017-04-22 21:49:04 +0800

    修正<a>bug增加页脚

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.writeln("<div id=\'footer\'>");
+document.writeln("            <div id=\'footertable\'>                ");
+document.writeln("                <ul>   ");
+document.writeln("                    <li id=\'mianzhu\'><a href=\'#\'>眠猪网 © 2017</a></li>");
+document.writeln("                    <li id=\'xiehao\'><a href=\'http://xiehao.me\' target=\'_blank\'>点这里去看蟹耗</a></li>");
+document.writeln("                </ul>                ");
+document.writeln("            </div>");
+document.writeln("        </div>     ");
